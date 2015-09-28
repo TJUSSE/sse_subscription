fd1266319e18b49abcfac10fd9389369052df8d6
blackbbc <505968815@qq.com>
2015-09-28 13:52:14 +0800

    validata email in the back-end and return send status

--- templates/sse_subscription_enter.tpl.php
+++ templates/sse_subscription_enter.tpl.php
@@ -7,7 +7,6 @@
 
 <div class="main-container" style="height: 250px">
     <div class="center-box">
-        <h2>订阅通知</h2>
         <form action="javascript:void(0);" method="post" accept-charset="utf-8" class="sub-form">
             <p class="user-mail">我们提供基于邮箱的通知订阅：<br>每天晚上6点左右，当天新通知将一起发送到您的邮箱中。
                 请留下您的邮箱，我们将发送给您一封订阅确认邮件。点击邮件中的链接，即可自定义期望收到的通知类型，并完成订阅。</p>
